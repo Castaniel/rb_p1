7e74cc7c997f61331f13f82cbc5d74b4c3e8821d
Marek <yurchylyak@gmail.com>
2018-04-03 13:32:38 +0300

    rb_p1

---
+++ index.rb
@@ -0,0 +1,82 @@
+# sdsd
+# b = 4
+# puts 4, 5.0, 'text'
+
+# a = 2
+# b = 3.0
+# c = 'text'
+# puts a, b , c
+# 2b + c
+# puts a + b
+# puts a +
+#	b
+# puts a
+#	+ b
+# puts 5 / 2
+# puts 5.0 / 2
+# puts 5 / 0
+# puts 5.0 / 0
+# puts 5 % 2
+# d = 4
+# d ** 2
+# f = '/text'
+# puts f
+# puts "someone says '#{f}'"
+# puts "someone  \
+#  says #{f}" 
+# puts %Q("silly #{f}")
+# a = 'test'
+# b = 'test'
+# puts a + ' ' + b
+# puts a + 5
+# puts a == b
+# a = 'text'
+# puts a * 33
+# a << ' test'
+# puts a
+# a = 'I love Ruby'
+# puts a[0], a[-4]
+# puts a[0], a[a.size-4]
+# a = 'Ruby'
+# puts a[6]
+# a = 'Hello Ruby'
+# puts a[6..10], a[-4..-1]
+# a = 'Ruby is awesome'
+# a[4,3] = ""
+# puts a
+# a = 'Ruby'
+# puts a.downcase
+# a = 'Ruby courses'
+# puts a.length
+# a = 'Ruby'
+# puts a.reverse
+# def method_man(param1)
+# end
+# puts 0.4 - 0.1 == 0.1
+# puts sprintf("number % .5f", Math::PI)
+# puts "\u20ac"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
